7abdb544b12369e3463ccfd7dc3af5b1264eb9d7
carolcortes <caroline.ocortes@gmail.com>
2022-08-22 23:56:30 -0300

    estrutura valores no estado

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,12 @@ import searchIcon from '../../images/searchIcon.svg';
 
 export default function Header({ pageTitle, searchIconRender = true }) {
   const [showSearchInput, setShowSearchInput] = useState(false);
+  const [searchInput, setSearch] = useState({ search: '', radio: '' });
+
+  const handleSearchInputChange = ({ target }) => {
+    const { name, value } = target;
+    setSearch((prevState) => ({ ...prevState, [name]: value }));
+  };
 
   const onSearchIconClick = () => {
     setShowSearchInput(!showSearchInput);
@@ -28,31 +34,49 @@ export default function Header({ pageTitle, searchIconRender = true }) {
         )}
         { showSearchInput && (
           <div>
-            <input type="text" name="search" data-testid="search-input" />
+            <input
+              value={ searchInput.search }
+              type="text"
+              name="search"
+              data-testid="search-input"
+              onChange={ handleSearchInputChange }
+            />
             <label htmlFor="search-radio">
-              Ingrediente
               <input
-                value="ingrediente"
+                value="ingredient"
                 id="search-radio"
+                name="radio"
                 type="radio"
                 data-testid="ingredient-search-radio"
+                onChange={ handleSearchInputChange }
+              />
+              Ingrediente
+              <input
+                value="name"
+                id="search-radio"
+                name="radio"
+                type="radio"
+                data-testid="name-search-radio"
+                onChange={ handleSearchInputChange }
               />
               Nome
               <input
-                value="nome"
-                id="search-radio"
-                type="radio"
-                data-testid="name-search-radio"
-              />
-              Primeira letra
-              <input
                 value="first-letter"
                 id="search-radio"
+                name="radio"
                 type="radio"
                 data-testid="first-letter-search-radio"
+                onChange={ handleSearchInputChange }
               />
+              Primeira letra
             </label>
-            <button type="button" data-testid="exec-search-btn">Buscar</button>
+            <button
+              type="button"
+              data-testid="exec-search-btn"
+              onClick={ () => {} }
+            >
+              Buscar
+            </button>
           </div>
         )}
         <p data-testid="page-title">{pageTitle}</p>
