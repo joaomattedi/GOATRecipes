d89b42710dbd01f0ae78bfa2d635068f3181f1bc
Joao <joaommattedi@gmail.com>
2022-08-24 14:18:33 -0300

    feature: checkbox ingredients that could be used for see user progress

--- src/pages/RecipeInProgress.jsx
+++ src/pages/RecipeInProgress.jsx
@@ -5,6 +5,7 @@ import fetchAPI from '../services/fetchAPI';
 
 export default function RecipeInProgress({ drink = false }) {
   const [recipe, setRecipe] = useState({});
+  const [checkedIngredients, setCheckedIngredients] = useState([]);
   const { id } = useParams();
   const regExIngredients = new RegExp('strIngredient', 'gi');
   const ingredients = Object.keys(recipe)
@@ -20,6 +21,15 @@ export default function RecipeInProgress({ drink = false }) {
     fetchAPI(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`).then((data) => setRecipe(data.meals[0]));
     // eslint-disable-next-line
   }, []);
+
+  const toggleChecked = (e) => {
+    if (checkedIngredients.includes(e.target.id)) {
+      const newChecked = checkedIngredients.filter((element) => element !== e.target.id);
+      return setCheckedIngredients(newChecked);
+    }
+    setCheckedIngredients([...checkedIngredients, e.target.id]);
+  };
+
   if (drink) {
     return (
       <div>
@@ -39,11 +49,16 @@ export default function RecipeInProgress({ drink = false }) {
             key={ element }
             htmlFor={ element }
             data-testid={ `${index}-ingredient-step` }
+            style={
+              checkedIngredients.includes(element) && 'text-decoration:line-through'
+            }
           >
             <input
               type="checkbox"
               id={ element }
               key={ element }
+              onChange={ (e) => toggleChecked(e) }
+              checked={ checkedIngredients.includes(element) }
             />
             {recipe[element]}
           </label>
@@ -71,17 +86,26 @@ export default function RecipeInProgress({ drink = false }) {
           key={ element }
           htmlFor={ element }
           data-testid={ `${index}-ingredient-step` }
+          className={ checkedIngredients.includes(element) && 'cu' }
         >
           <input
             type="checkbox"
             id={ element }
             key={ element }
+            onChange={ (e) => toggleChecked(e) }
+            checked={ checkedIngredients.includes(element) }
           />
           {recipe[element]}
         </label>
       ))}
       <p data-testid="instructions">{recipe.strInstructions}</p>
-      <button type="button" data-testid="finish-recipe-btn">Finalizar</button>
+      <button
+        type="button"
+        data-testid="finish-recipe-btn"
+        disabled={ ingredients.length !== checkedIngredients }
+      >
+        Finalizar
+      </button>
     </div>
   );
 }
