5a365806abed527d4c5b0f81ee75702e379b96ea
Lis Vieira Silva <lisvs10@gmail.com>
2022-08-24 15:55:04 -0300

    Cria Profile e adiciona Header a pagina Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import LoginForm from '../components/Login/LoginForm';
+import Header from '../components/Header/Header';
 
 export default function Login() {
-  return <LoginForm />;
+  return (
+    <div>
+      <Header pageTitle="Profile" searchIconRender />
+      <LoginForm />
+    </div>);
 }
--- src/tests/helper/renderWithRouter.js
+++ src/tests/helper/renderWithRouter.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
-// import Context from '../../Context/Context';
 
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
