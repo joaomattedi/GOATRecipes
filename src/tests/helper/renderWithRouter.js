3967497a882e30903d58a82d3556d161c6dc61ec
Lis Vieira Silva <lisvs10@gmail.com>
2022-08-24 15:17:47 -0300

    Corrige renderWithRouter

--- src/tests/helper/renderWithRouter.js
+++ src/tests/helper/renderWithRouter.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
-import ProviderContext from '../../Context/ProviderContext';
+// import Context from '../../Context/Context';
 
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
   return ({
     ...render(
-      <ProviderContext>
         <Router history={ history }>{component}</Router>
-      </ProviderContext>
     ), history,
   });
 };
